a859fbdcb924807d8e0b7c9947758da105cab9be
pgorlani <paolo.gorlani@codeplay.com>
2023-08-29 15:28:10 +0100

    update test

--- test/unittest/blas3/blas3_gemm_test.cpp
+++ test/unittest/blas3/blas3_gemm_test.cpp
@@ -31,16 +31,16 @@ template <typename scalar_t>
 const auto mytest = ::testing::Combine(
     ::testing::Values(0),                          // offset
     ::testing::Values(1),                          // batch
-    ::testing::Values(8192),                 // m
-    ::testing::Values(8192),                 // n
-    ::testing::Values(8192),                     // k
-    ::testing::Values('n'),                   // transa
-    ::testing::Values('n'),                   // transb
+    ::testing::Values(64, 100, 128, 444, 512, 777, 2048, 8192),  // m
+    ::testing::Values(64, 100, 128, 444, 512, 777, 2048, 8192),  // m
+    ::testing::Values(20, 40, 600, 1899),                     // k
+    ::testing::Values('n','t'),                   // transa
+    ::testing::Values('n','t'),                   // transb
     ::testing::Values<scalar_t>(1),              // alpha
-    ::testing::Values<scalar_t>(0),              // beta
-    ::testing::Values(1),                          // lda_mul
-    ::testing::Values(1),                          // ldb_mul
-    ::testing::Values(1),                          // ldc_mul
+    ::testing::Values<scalar_t>(1),              // beta
+    ::testing::Values(4),                          // lda_mul
+    ::testing::Values(2),                          // ldb_mul
+    ::testing::Values(3),                          // ldc_mul
     ::testing::Values(gemm_batch_type_t::strided)  // batch_type
 );
 GENERATE_GEMM_TEST(Gemm, mytest);
