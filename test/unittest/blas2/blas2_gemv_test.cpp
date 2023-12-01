24fda89aa08221b5f686258d645bbcf867d2c75c
pgorlani <paolo.gorlani@codeplay.com>
2023-12-01 16:47:37 +0000

    mod gemv test

--- test/unittest/blas2/blas2_gemv_test.cpp
+++ test/unittest/blas2/blas2_gemv_test.cpp
@@ -42,64 +42,89 @@ void run_test(const combination_t<scalar_t> combi) {
   index_t lda_mul;
   std::tie(alloc, m, n, alpha, beta, trans, incX, incY, lda_mul) = combi;
 
+
+  const int reps = 10;
+  const char* t_str = trans ? "t" : "n";
+
+  int a_size = m * n * lda_mul;
+  int x_size = trans ? (1 + (m - 1) * incX) : (1 + (n - 1) * incX);
+  int y_size = trans ? (1 + (n - 1) * incY) : (1 + (m - 1) * incY);
+
+  std::vector< std::vector<scalar_t> > y_v_cpu(reps);
+  std::vector< std::vector<scalar_t> > a_m(reps);
+  std::vector< std::vector<scalar_t> > x_v(reps);
+
+  std::vector<  typename blas::helper::AllocHelper<scalar_t, mem_alloc>::type > v_y_gpu(reps);
+  std::vector<  typename blas::helper::AllocHelper<scalar_t, mem_alloc>::type > v_x_gpu(reps);
+  std::vector<  typename blas::helper::AllocHelper<scalar_t, mem_alloc>::type > m_a_gpu(reps);
+
   auto q = make_queue();
+  blas::SB_Handle sb_handle(make_mp());
 
-  for (int i = 0; i < 10; ++i) {
-    std::cerr << ".";
+  std::vector<cl::sycl::event > wait_list;
 
-    const char* t_str = trans ? "t" : "n";
-
-    int a_size = m * n * lda_mul;
-    int x_size = trans ? (1 + (m - 1) * incX) : (1 + (n - 1) * incX);
-    int y_size = trans ? (1 + (n - 1) * incY) : (1 + (m - 1) * incY);
-
+  for (int r = 0; r < reps; ++r) {
     // Input matrix
-    std::vector<scalar_t> a_m(a_size, 1.0);
+    a_m[r] = std::vector<scalar_t> (a_size, 1.0);
     // Input Vector
-    std::vector<scalar_t> x_v(x_size, 1.0);
-    // output Vector
-    std::vector<scalar_t> y_v_gpu_result(y_size, scalar_t(10.0));
-    // output system vector
-    std::vector<scalar_t> y_v_cpu(y_size, scalar_t(10.0));
+    x_v[r] = std::vector<scalar_t> (x_size, 1.0);
+    y_v_cpu[r] =  std::vector<scalar_t>(y_size, scalar_t(10.0)); 
 
-    fill_random(a_m);
-    fill_random(x_v);
+    fill_random(a_m[r]);
+    fill_random(x_v[r]);
 
     // SYSTEM GEMV
-    reference_blas::gemv(t_str, m, n, alpha, a_m.data(), lda_mul * m,
-                         x_v.data(), incX, beta, y_v_cpu.data(), incY);
+    reference_blas::gemv(t_str, m, n, alpha, a_m[r].data(), lda_mul * m,
+                         x_v[r].data(), incX, beta, y_v_cpu[r].data(), incY);
 
-    blas::SB_Handle sb_handle(make_mp());
-    auto m_a_gpu = helper::allocate<mem_alloc, scalar_t>(a_size, q);
-    auto v_x_gpu = helper::allocate<mem_alloc, scalar_t>(x_size, q);
-    auto v_y_gpu = helper::allocate<mem_alloc, scalar_t>(y_size, q);
+  }
+
+
+  for (int r = 0; r < reps; ++r) {
+
+    v_y_gpu[r] = helper::allocate<mem_alloc, scalar_t>(y_size, q);
+
+    std::cerr << ".";
+
+   // output Vector
+    std::vector<scalar_t> y_v_gpu_result(y_size, scalar_t(10.0));
+    // output system vector
+
+    m_a_gpu[r] = helper::allocate<mem_alloc, scalar_t>(a_size, q);
+    v_x_gpu[r] = helper::allocate<mem_alloc, scalar_t>(x_size, q);
 
     auto copy_m =
-        helper::copy_to_device<scalar_t>(q, a_m.data(), m_a_gpu, a_size);
+        helper::copy_to_device<scalar_t>(q, a_m[r].data(), m_a_gpu[r], a_size);
     auto copy_x =
-        helper::copy_to_device<scalar_t>(q, x_v.data(), v_x_gpu, x_size);
+        helper::copy_to_device<scalar_t>(q, x_v[r].data(), v_x_gpu[r], x_size);
     auto copy_y = helper::copy_to_device<scalar_t>(q, y_v_gpu_result.data(),
-                                                   v_y_gpu, y_size);
+                                                   v_y_gpu[r], y_size);
 
     sb_handle.wait({copy_m, copy_x, copy_y});
 
     // SYCLGEMV
-    auto gemv_event = _gemv(sb_handle, *t_str, m, n, alpha, m_a_gpu,
-                            lda_mul * m, v_x_gpu, incX, beta, v_y_gpu, incY);
-    sb_handle.wait(gemv_event);
+    auto gemv_event = _gemv(sb_handle, *t_str, m, n, alpha, m_a_gpu[r],
+                            lda_mul * m, v_x_gpu[r], incX, beta, v_y_gpu[r], incY, {copy_m, copy_x, copy_y});
+    //sb_handle.wait(gemv_event);
 
+    wait_list = concatenate_vectors(wait_list, gemv_event);
+  }
+
+  sb_handle.wait(wait_list);
+
+  for (int r = reps-1; r > 0; --r) {
+    std::vector<scalar_t> y_v_gpu_result(y_size, scalar_t(10.0));
     auto event =
-        blas::helper::copy_to_host(q, v_y_gpu, y_v_gpu_result.data(), y_size);
-    sb_handle.wait(event);
+        blas::helper::copy_to_host(q, v_y_gpu[r], y_v_gpu_result.data(), y_size);
+    sb_handle.wait({event});
 
-    const bool isAlmostEqual = utils::compare_vectors(y_v_gpu_result, y_v_cpu);
+    const bool isAlmostEqual = utils::compare_vectors(y_v_gpu_result, y_v_cpu[r]);
     ASSERT_TRUE(isAlmostEqual);
 
-    helper::deallocate<mem_alloc>(m_a_gpu, q);
-    helper::deallocate<mem_alloc>(v_x_gpu, q);
-    helper::deallocate<mem_alloc>(v_y_gpu, q);
+    helper::deallocate<mem_alloc>(m_a_gpu[r], q);
+    helper::deallocate<mem_alloc>(v_x_gpu[r], q);
+    helper::deallocate<mem_alloc>(v_y_gpu[r], q);
   }
-  //  q.wait();
 }
 
 template <typename scalar_t>
@@ -145,8 +170,8 @@ const auto combi =
 template <typename scalar_t>
 const auto combi =
     ::testing::Combine(::testing::Values("usm", "buf"),   // allocation type
-                       ::testing::Values(11, 1023),       // m
-                       ::testing::Values(14, 1010),       // n
+                       ::testing::Values(2048),       // m
+                       ::testing::Values(2048),       // n
                        ::testing::Values<scalar_t>(1.5),  // alpha
                        ::testing::Values<scalar_t>(0.0, 1.5),  // beta
                        ::testing::Values(false, true),         // trans
